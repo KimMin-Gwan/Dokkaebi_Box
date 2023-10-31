33073966f92a3588ea1a6a66e933915675a485bb
ANTL-KJH <wogus9190@gmail.com>
2023-10-31 14:21:12 +0900

    Update Magnetic

---
+++ Develop/jaehyun/test/MagneticSensor/MagneticSensor.py
@@ -0,0 +1,30 @@
+"""
+* Project : 2023 Seoul AIOT Hackathon
+* Program Purpose and Features :
+* - class MagneticSensor
+* Author : JH KIM
+* First Write Date : 2023.11.03
+* ==========================================================================
+* Program history
+* ==========================================================================
+* Author    		Date		    Version		History
+* JH KIM            2023.11.03		v1.00		First Write
+"""
+
+import RPi.GPIO as GPIO #import the GPIO library
+import time
+
+class clsMagneticSensor:
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.doorState = False # False(닫힘)/True(열림)
+
+    def runMagneticSensor(self):
+        while True:
+            if GPIO.input(8):
+               print("Door is open")
+               time.sleep(1)
+            if GPIO.input(8) == False:
+               print("Door is closed")
+               time.sleep(1)
---
+++ Develop/jaehyun/test/MagneticSensor/magneticConstant.py
@@ -0,0 +1,13 @@
+"""
+* Project : 2023 Seoul AIOT Hackathon
+* Program Purpose and Features :
+* - MagneticSensor Constant
+* Author : JH KIM
+* First Write Date : 2023.11.03
+* ==========================================================================
+* Program history
+* ==========================================================================
+* Author    		Date		    Version		History
+* JH KIM            2023.11.03		v1.00		First Write
+"""
+MAGNETIC_DEFAULT_PIN = 8
