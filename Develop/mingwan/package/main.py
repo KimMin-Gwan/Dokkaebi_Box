0c0a3a4881df1045235309dbd049ca3d795b19aa
김민관 <alsrhks2508@naver.com>
2023-11-01 04:32:49 +0900

    ver 0.5

--- Develop/mingwan/package/main.py
+++ Develop/mingwan/package/main.py
@@ -1,7 +1,18 @@
-from package.Model import *
+from fastapi import FastAPI
+from View import AppServer
+from Controller import Web_Controller
+
+class Main:
+    def __init__(self):
+        self.app = FastAPI()
+        self.web_controller = Web_Controller()
+        self.app_server = AppServer(self.app, self.web_controller)
+
+
 
 def main():
-    test = testClass()
+    main_cls = Main()
+
 
 
 if __name__ == "__main__":
---
+++ Develop/mingwan/test/my-react-app/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from "react-dom";
+import MainPage from "./MainPage";
+import React, { useState, useEffect } from "react";
+
+
+/*
+function App() {
+  const[message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+    .then((response) => response.json())
+    .then((data) => setMessage(data.message))
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  )
+}
+*/
+function App() {
+  return (
+    <div>
+      <MainPage /> {/* MainPage 컴포넌트를 렌더링 */}
+    </div>
+  );
+}
+
+
+export default App;
