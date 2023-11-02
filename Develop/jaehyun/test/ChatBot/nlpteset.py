89590adb7698ed8f024c9fcc3a8e25e740d4a8d1
ANTL-KJH <wogus9190@gmail.com>
2023-11-02 10:23:50 +0900

    Update nlpteset.py

--- Develop/jaehyun/test/ChatBot/nlpteset.py
+++ Develop/jaehyun/test/ChatBot/nlpteset.py
@@ -1,14 +1,12 @@
-from konlpy.tag import Hannanum
+from konlpy.tag import Hannanum, Okt
 
 hannanum = Hannanum()
-test_txt= "11월13일 13시30분"
+Okt = Okt()
+test_txt= "11월 13일 13시 13분"
 print(hannanum.analyze(test_txt))
-
-
 print(hannanum.morphs(test_txt))
-
-
 print(hannanum.nouns(test_txt))
-
-
 print(hannanum.pos(test_txt))
+
+print(Okt.morphs(test_txt))
+print(Okt.nouns(test_txt))
